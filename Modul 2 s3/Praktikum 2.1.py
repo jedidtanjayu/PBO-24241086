1588180b9b2ef24d6d21d83b539a818514b59647
jedidtanjayu <jedidtanjayu@gmail.com>
2025-12-17 09:17:28 +0800

    Add files via upload

---
+++ Modul 2 s3/Praktikum 2.1.py
@@ -0,0 +1,12 @@
+class RekeningBank:
+    def __init__(self, nama, saldo):
+        self.nama = nama
+        self__privatesaldo = saldo 
+
+
+akun_budi = RekeningBank("Budi", 1000000)
+print(f"Saldo awal Budi: {akun_budi.saldo}")
+try
+    print(f"Saldo akhir Budi: {akun_budi.saldo}")
+akun_budi.saldo = -5000000
+print(f"Saldo akhir Budi: {akun_budi.saldo}")
